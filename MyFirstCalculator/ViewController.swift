ca6009f28f78bd8a375ee28d6851a844da690075
Isla <Islajiin@gmail.com>
2025-03-15 09:24:59 +0900

    feat : 기본 구성 outlet과 action 연결

--- MyFirstCalculator/ViewController.swift
+++ MyFirstCalculator/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstOperandField: UITextField!
+    
+    @IBAction func selectOperator(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var operatorButton: UIButton!
+    
+    @IBOutlet weak var secondOperandField: UITextField!
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func calculator(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view}
     }
-
-
 }
 
