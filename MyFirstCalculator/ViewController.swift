d0a1fbc523bf0be60b07ecf5e6dce5361b039efb
Isla <Islajiin@gmail.com>
2025-03-17 08:33:37 +0900

    feat : 기능 actionsheet추가
    각 기능에 actionsheet를 추가함
    사용자로 부터 계산을 받는 곳에 타입 컨버전을 실시

--- MyFirstCalculator/ViewController.swift
+++ MyFirstCalculator/ViewController.swift
@@ -12,9 +12,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstOperandField: UITextField!
     
     @IBAction func selectOperator(_ sender: Any) {
+        
+        let actionSheet=UIAlertController(title: nil, message: nil , preferredStyle: .actionSheet)
+        
+        let plusAction=UIAlertAction(title: "+ (더하기)", style: .default) { _ in
+            
+            self.operatorButton.setTitle("+", for: .normal)
+            
+        }
+        actionSheet.addAction(plusAction)
+        
+        let minusAction=UIAlertAction(title: "-(빼기)", style: .default) { _ in
+            self.operatorButton.setTitle("-", for: .normal)}
+        actionSheet.addAction(minusAction)
+        
+        let divideAction=UIAlertAction(title: "/(나누기)", style: .default) { _ in
+            self.operatorButton.setTitle("/", for: .normal)
+        }
+        actionSheet.addAction(divideAction)
+        
+        let multiplyAction=UIAlertAction(title: "*(곱하기)", style: .default) { _ in
+            self.operatorButton.setTitle("*", for: .normal)
+        }
+        actionSheet.addAction(multiplyAction)
+        
+        present(actionSheet, animated: true)
+        
     }
-    
+  
+        
+        
+
     @IBOutlet weak var operatorButton: UIButton!
+    
     
     @IBOutlet weak var secondOperandField: UITextField!
     
@@ -22,6 +52,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func calculator(_ sender: Any) {
+        let a=Int( firstOperandField.text!)!
+        let b=Int( secondOperandField.text!)!
+        
+        let result = a+b
+        resultLabel.text = "\(result)"
+        
+        
+        
     }
     
     
